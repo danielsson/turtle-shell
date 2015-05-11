1b60d16d5d72b629182e52d111fb9450f3eb3703
Mattias Danielsson <matdanie@kth.se>
2015-05-11 14:12:59 +0200

    Implemented KALAS directives.

--- main.c
+++ main.c
@@ -2,20 +2,61 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
 
 #define TRUE 1
 #define FALSE 0
+#define CMD_LEN 80
+
+void cmd_cd();
+void cmd_exit();
+
+/*
+ * Tokenize the command into a char
+ */
+void tokenize(char * command) {
+
+}
 
 
+
+
+/**
+ * This function is where the proverbial magic sauce lives.
+ */
 void forker(const char * command) {
 
-    int pid;
+    pid_t pid;
     pid = fork();
     if ( pid == 0 ) {
         /* Child */
+        char * args[1];
+        args[0] = NULL;
+
+        if(strlen(command) > 1) {
+            if(execvp(command, args) == -1) {
+                printf("Unknown Command: %s\n", command);
+                exit(1);
+            }
+        }
+
+        exit(1);
 
     } else if(pid > 0) {
         /* Parent */
+        int status = 0;
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid failed\n");
+        } else {
+            if(WIFEXITED(status)) {
+                printf("Exited with status %d\n", WEXITSTATUS(status));
+            } else if(WIFSIGNALED(status)) {
+                printf("Exited through signal %d\n", WTERMSIG(status));
+            } else {
+                printf("Exited abnormally\n");
+            }
+        }
 
     } else {
         /* System fork err */
@@ -24,22 +65,53 @@ void forker(const char * command) {
     }
 }
 
+void remove_trailing_nl(char * str) {
+    size_t len;
+    len = strlen(str);
+
+    str[len-1] = 0;
+}
+
+int handle_builtin(char * str) {
+
+    if (strcmp(str, BI_CD)) {
+        cmd_cd();
+    } else if(strcmp(str, BI_EXIT)) {
+        cmd_exit();
+    }
+
+    return 0;
+}
+
+void cmd_cd() {
+
+}
+
+void cmd_exit() {
+
+}
+
 int main(int argc, const char * argv[]) {
 
     int is_running = TRUE;
-    char command[70];
-    char hostname[70];
+    char command[CMD_LEN];
+    char hostname[CMD_LEN];
 
-    gethostname(hostname, 70);
+    gethostname(hostname, CMD_LEN);
 
 
     while (is_running) {
 
         /* Command prompt */
         printf("turtle-shell@%s %% ", hostname);
-        fgets(command, 70, stdin);
+        fgets(command, CMD_LEN, stdin);
+        remove_trailing_nl(command);
+
 
         /* Now the fun stuff */
+
+
+
         forker(command);
 
     }
--- main.h
+++ main.h
@@ -3,7 +3,10 @@
 #ifndef TURTLE_SHELL_MAIN_H
 #define TURTLE_SHELL_MAIN_H
 
-
+/* builtins */
+static const char * BI_CD = "cd";
+static const char * BI_EXIT = "exit";
+static const char * BI_CHKENV = "checkEnv";
 
 
 #endif
