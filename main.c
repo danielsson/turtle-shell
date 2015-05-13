59e94b8acbab5fbc32d4f308efd759c753b2d50a
Mattias Danielsson <matdanie@kth.se>
2015-05-13 14:29:55 +0200

    Added some bg stuff

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <dirent.h>
 #include <signal.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <sys/errno.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -12,6 +15,8 @@
 #define ARGS_SIZE 5
 
 int is_running = TRUE;
+
+void run_child(char *const *args, const char *command);
 
 int count_non_null(char *args[ARGS_SIZE]) {
     int count;
@@ -102,24 +107,31 @@ void foreground(char *args[ARGS_SIZE]) {
     pid = fork();
     if (pid == 0) {
         /* Child */
-
-        if (strcmp(args[0], BI_LS) == 0) {
-            cmd_ls();
-        } else {
-            if (execvp(command, args) == -1) {
-                printf("Unknown Command: %s\n", command);
-                exit(1);
-            }
-        }
-
-        exit(99);
+        run_child(args, command);
 
     } else if (pid > 0) {
         /* Parent */
         int status = 0;
+        struct rusage before;
+        struct rusage after;
+        struct timeval diff;
+
+        if(getrusage(RUSAGE_CHILDREN, &before) == -1) {
+            perror("Catastrophic failure");
+            exit(EXIT_FAILURE);
+        }
+
         if (waitpid(pid, &status, 0) == -1) {
             perror("waitpid failed\n");
         } else {
+                getrusage(RUSAGE_CHILDREN, &after);
+
+                timersub(&after.ru_stime, &before.ru_stime, &diff);
+                printf("System time: %ld.%05i\n", diff.tv_sec, diff.tv_usec);
+
+                timersub(&after.ru_utime, &before.ru_utime, &diff);
+                printf("User time: %ld.%05i\n", diff.tv_sec, diff.tv_usec);
+
             if (WIFEXITED(status)) {
                 printf("Exited with status %d\n", WEXITSTATUS(status));
             } else if (WIFSIGNALED(status)) {
@@ -136,10 +148,61 @@ void foreground(char *args[ARGS_SIZE]) {
     }
 }
 
+
+void background(char * args[ARGS_SIZE]) {
+
+    char * command = args[0];
+
+    pid_t pid;
+    pid = fork();
+
+    if (pid == 0) {
+        run_child(args, command);
+        return;
+
+    } else (pid < 0){
+        /* System fork err */
+        printf("Fork failed");
+        exit(0xCC);
+    }
+}
+
+void run_child(char *const *args, const char *command) {/* Child */
+
+    if (strcmp(args[0], BI_LS) == 0) {
+            cmd_ls();
+        } else {
+            if (execvp(command, args) == -1) {
+                printf("Unknown Command: %s\n", command);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+    exit(99);
+}
+
+void poll_background_children() {
+    struct rusage before;
+    struct rusage after;
+    struct timeval diff;
+
+    int status = 0;
+
+
+    waitpid(-1, &status, WNOHANG);
+
+    if (status != NULL) {
+
+    }
+
+}
+
+
 int main(int argc, char *argv[]) {
 
     char command[CMD_LEN];
     char *args[ARGS_SIZE] = {0};
+    int len;
 
     while (is_running) {
         /* Command prompt */
@@ -149,10 +212,15 @@ int main(int argc, char *argv[]) {
         tokenize(command, args);
 
         /* Now the fun stuff */
+
+        len = count_non_null(args);
+        if(*(args[len-1]) == '&') {
+            background(args);
+        }
+
         if (!handle_builtin(args))
             foreground(args);
     }
 
     return 0;
 }
-
