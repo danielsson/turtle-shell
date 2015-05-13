c5fc8f905ef8ed2736431b93f61126e64f659e8c
Lovisa R <lrunhem@gmail.com>
2015-05-13 16:26:04 +0200

    Added comments. Polling is working, and background processes are correctly handled. Also a giant squid entered the code.

--- main.c
+++ main.c
@@ -24,6 +24,19 @@ int count_non_null(char *args[ARGS_SIZE]) {
     return count;
 }
 
+/**
+ * Print the system and user time for a terminated process.
+ */
+void print_time(struct rusage *before, struct rusage *after) {
+    struct timeval diff;
+
+    timersub(&after->ru_stime, &before->ru_stime, &diff);
+    printf("System time: %ld.%05i\n", diff.tv_sec, diff.tv_usec);
+
+    timersub(&after->ru_utime, &before->ru_utime, &diff);
+    printf("User time: %ld.%05i\n", diff.tv_sec, diff.tv_usec);
+}
+
 
 /*
  * Splits a string into an array of strings with single space as delimiter.
@@ -45,7 +58,9 @@ void tokenize(char *command, char *into[ARGS_SIZE]) {
     }
 }
 
-
+/**
+ * Remove the nl character from the arguments.
+ */
 void remove_trailing_nl(char *str) {
     size_t len;
     len = strlen(str);
@@ -53,6 +68,9 @@ void remove_trailing_nl(char *str) {
     str[len - 1] = 0;
 }
 
+/**
+ * Execute the ls command.
+ */
 void cmd_ls() {
     DIR *dir;
     struct dirent *ep;
@@ -69,19 +87,27 @@ void cmd_ls() {
     exit(EXIT_SUCCESS);
 }
 
-
+/**
+ * Execute the exit command.
+ */
 void cmd_exit() {
     printf("exiting\n");
     kill(-2, SIGTERM);
     is_running = FALSE;
 }
 
+/**
+ * Execute the cd command.
+ */
 void cmd_cd(char *args[ARGS_SIZE]) {
     if (chdir(args[1]) == -1) {
         perror("Failed to chdir");
     }
 }
 
+/**
+ * Handle the built in commands: cd and exit.
+ */
 int handle_builtin(char *args[ARGS_SIZE]) {
 
     if (strcmp(args[0], BI_CD) == 0) {
@@ -114,9 +140,8 @@ void foreground(char *args[ARGS_SIZE]) {
         int status = 0;
         struct rusage before;
         struct rusage after;
-        struct timeval diff;
 
-        if(getrusage(RUSAGE_CHILDREN, &before) == -1) {
+        if (getrusage(RUSAGE_CHILDREN, &before) == -1) {
             perror("Catastrophic failure");
             exit(EXIT_FAILURE);
         }
@@ -124,13 +149,9 @@ void foreground(char *args[ARGS_SIZE]) {
         if (waitpid(pid, &status, 0) == -1) {
             perror("waitpid failed\n");
         } else {
-                getrusage(RUSAGE_CHILDREN, &after);
+            getrusage(RUSAGE_CHILDREN, &after);
 
-                timersub(&after.ru_stime, &before.ru_stime, &diff);
-                printf("System time: %ld.%05i\n", diff.tv_sec, diff.tv_usec);
-
-                timersub(&after.ru_utime, &before.ru_utime, &diff);
-                printf("User time: %ld.%05i\n", diff.tv_sec, diff.tv_usec);
+            print_time(&before, &after);
 
             if (WIFEXITED(status)) {
                 printf("Exited with status %d\n", WEXITSTATUS(status));
@@ -148,10 +169,12 @@ void foreground(char *args[ARGS_SIZE]) {
     }
 }
 
+/**
+ * Start a background process.
+ */
+void background(char *args[ARGS_SIZE]) {
 
-void background(char * args[ARGS_SIZE]) {
-
-    char * command = args[0];
+    char *command = args[0];
 
     pid_t pid;
     pid = fork();
@@ -160,40 +183,69 @@ void background(char * args[ARGS_SIZE]) {
         run_child(args, command);
         return;
 
-    } else (pid < 0){
+    } else if (pid < 0) {
         /* System fork err */
         printf("Fork failed");
         exit(0xCC);
     }
 }
 
+/**
+ * Start a child process.
+ */
 void run_child(char *const *args, const char *command) {/* Child */
-
+    int i;
     if (strcmp(args[0], BI_LS) == 0) {
-            cmd_ls();
-        } else {
-            if (execvp(command, args) == -1) {
-                printf("Unknown Command: %s\n", command);
-                exit(EXIT_FAILURE);
+        cmd_ls();
+    } else {
+        if (execvp(command, args) == -1) {
+            printf("Unknown Command: %s\n", command);
+
+            for (i = 0; i < ARGS_SIZE; i++) {
+                puts(args[i]);
             }
+
+            exit(EXIT_FAILURE);
         }
+    }
 
     exit(99);
 }
 
+/**
+ * Poll for any terminated background processes.
+ */
+
 void poll_background_children() {
     struct rusage before;
     struct rusage after;
-    struct timeval diff;
 
     int status = 0;
 
-
+    getrusage(RUSAGE_CHILDREN, &before);
     waitpid(-1, &status, WNOHANG);
+    printf("Status: %d \n", status);
+    getrusage(RUSAGE_CHILDREN, &after);
 
-    if (status != NULL) {
+    if (status != 0) {
+        if (WIFEXITED(status)) {
+            print_time(&before, &after);
+            printf("Exited with status %d\n", WEXITSTATUS(status));
 
+
+        } else if (WIFSIGNALED(status)) {
+            print_time(&before, &after);
+            printf("Exited through signal %d\n", WTERMSIG(status));
+
+
+        } else {
+            print_time(&before, &after);
+            puts("Exited abnormally");
+
+        }
     }
+
+    printf("Status2: %d \n", status);
 
 }
 
@@ -201,10 +253,14 @@ void poll_background_children() {
 int main(int argc, char *argv[]) {
 
     char command[CMD_LEN];
-    char *args[ARGS_SIZE] = {0};
     int len;
 
     while (is_running) {
+
+        char *args[ARGS_SIZE] = {0};
+
+        poll_background_children();
+
         /* Command prompt */
         printf(" ❤ ❤ ❤ ");
         fgets(command, CMD_LEN, stdin);
@@ -214,12 +270,14 @@ int main(int argc, char *argv[]) {
         /* Now the fun stuff */
 
         len = count_non_null(args);
-        if(*(args[len-1]) == '&') {
+        if (*(args[len - 1]) == '&') {
+            (args[len - 1]) = 0;
             background(args);
+
+        } else if (!handle_builtin(args)) {
+            foreground(args);
         }
 
-        if (!handle_builtin(args))
-            foreground(args);
     }
 
     return 0;
